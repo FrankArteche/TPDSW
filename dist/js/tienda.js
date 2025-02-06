3e7cdfd9185cc59468939735535c5b123c6b1c85
IvanaZandona <maribeliva2.0@gmail.com>
2025-02-06 19:17:36 -0300

    home, js para carousel, estilos

--- dist/js/tienda.js
+++ dist/js/tienda.js
@@ -7,3 +7,21 @@ rangeInput.addEventListener('input', (event) => {
     rangeMin.textContent = `$0`;
     rangeMax.textContent = `$${value.toLocaleString()}`;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // cierra todos los filtros al inicio
+    document.querySelectorAll('.accordion-collapse').forEach(function (collapse) {
+        collapse.classList.remove('show'); // Remueve la clase 'show'
+    });
+
+    // botones tambien cerrados
+    document.querySelectorAll('.accordion-button').forEach(function (button) {
+        button.classList.add('collapsed'); 
+        button.setAttribute('aria-expanded', 'false'); 
+    });
+
+    // abre solo el filtro de precio
+    document.getElementById("filter7").classList.add("show");
+    document.querySelector('button[data-bs-target="#filter7"]').classList.remove("collapsed");
+    document.querySelector('button[data-bs-target="#filter7"]').setAttribute("aria-expanded", "true");
+});
