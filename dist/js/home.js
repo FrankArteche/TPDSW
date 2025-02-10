14f5bf93412996bce978d8c479b87eb049ce2163
IvanaZandona <maribeliva2.0@gmail.com>
2025-02-09 23:33:18 -0300

    estilos detalle producto, seo de home y tienda

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,5 +1,31 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-})
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel1 = document.querySelector('#carousel-caption');
+  var carousel1 = new bootstrap.Carousel(myCarousel1, {
+    interval: 3000,
+    wrap: true
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel2 = document.querySelector('#carouselBotas');
+  var carousel2 = new bootstrap.Carousel(myCarousel2, {
+    interval: 3000,
+    wrap: true
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel3 = document.querySelector('#carouselRuedas');
+  var carousel3 = new bootstrap.Carousel(myCarousel3, {
+    interval: 5000,
+    wrap: true
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel4 = document.querySelector('#carouselAccRep');
+  var carousel4 = new bootstrap.Carousel(myCarousel4, {
+    interval: 7000,
+    wrap: true
+  });
+});
